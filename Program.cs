25867d29a6e9869903d0c66da90b737198815ad1
ANURAG-prakash <78632929+ANURAG-prakash@users.noreply.github.com>
2021-03-12 07:18:11 +0530

    Anurag Prakash has added usecase 1

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace AddressBookSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AddressBookMain();
+        }
+        public static void AddressBookMain()
+        {
+            Console.WriteLine("Welcome To Adress Book");
+        }
+    }
+}
